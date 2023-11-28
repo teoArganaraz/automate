c7a6461ea96c8ea10b1828107c3df6011d013a04
teoArganaraz <teoas04@gmail.com>
2023-11-28 16:02:47 -0300

    created basic assitant with langchain

---
+++ conversational_agent/agent/agent.py
@@ -0,0 +1,38 @@
+from langchain import OpenAI, LLMChain
+from langchain import PromptTemplate
+from langchain.memory import ConversationBufferWindowMemory
+from dotenv import find_dotenv, load_dotenv
+import requests
+from playsound import playsound
+import os
+
+load_dotenv(find_dotenv())
+
+def get_ai_response(human_input):
+
+    template = """
+    You are as a role of an AI assistant, now lets playing the following requirements:
+    1/ Your Name is Sofia, you are really devoted to help others to achive his goals.
+    2/ Your like to use the Polya Heuristics and the Feynman technique in order to solve problems
+
+    {history}
+    Person: {human_input}
+    Sofia: 
+    """
+    prompt = PromptTemplate(
+        input_variables = {"history","human_input"},
+        template = template
+    )
+
+    chatgpt_chain = LLMChain(
+        llm=OpenAI(temperature = 0),
+        prompt = prompt,
+        verbose = True,
+        memory = ConversationBufferWindowMemory(k=2)
+
+    )
+
+    output = chatgpt_chain.predict(human_input=human_input)
+
+    return output
+
