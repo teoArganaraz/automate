6323d032b9ffbf26721b4751edc65504e27df380
teoArganaraz <teoas04@gmail.com>
2023-12-10 21:27:03 -0300

    tool to scrape websites

---
+++ researcher_agent/agent/browserless_integration.py
@@ -0,0 +1,46 @@
+import requests
+import json
+from dotenv import load_dotenv
+import os
+from bs4 import BeautifulSoup
+from agent.agent import summary
+
+load_dotenv()
+browserless_api_key = os.getenv("BROWSERLESS_API_KEY")
+
+def scrape_website(objective: str, url: str):
+    # scrape website, and also will summarize the content based on objective if the content is too large
+    # objective is the original objective & task that user give to the agent, url is the url of the website to be scraped
+
+    print("Scraping website...")
+    # Define the headers for the request
+    headers = {
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json',
+    }
+
+    # Define the data to be sent in the request
+    data = {
+        "url": url
+    }
+
+    # Convert Python object to JSON string
+    data_json = json.dumps(data)
+
+    # Send the POST request
+    post_url = f"https://chrome.browserless.io/content?token={browserless_api_key}"
+    response = requests.post(post_url, headers=headers, data=data_json)
+
+    # Check the response status code
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+        text = soup.get_text()
+        print("CONTENTTTTTT:", text)
+
+        if len(text) > 10000:
+            output = summary(objective, text)
+            return output
+        else:
+            return text
+    else:
+        print(f"HTTP request failed with status code {response.status_code}")
