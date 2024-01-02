28b9afbc60d7c6e604c6666d8af509ba0dba5211
teoArganaraz <teoas04@gmail.com>
2024-01-02 17:02:41 -0300

    added basic request with OAuth 3-legged authentication

---
+++ linkedin-integration/scripts/basic_request.py
@@ -0,0 +1,18 @@
+from linkedin_api.clients.restli.client import RestliClient
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
+
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+restli_client = RestliClient()
+
+response = restli_client.get(
+  resource_path="/userinfo",
+  access_token=ACCESS_TOKEN
+)
+
+print(response.entity)
+
+if __name__ == "__main__":
+  print(response.entity)
